3b4b389c34f0d47a323b6a6e27d8427eac3cbc3a
NhomToongTenz <155875453+NhomToongTenz@users.noreply.github.com>
2024-12-04 06:38:31 +0700

    z

--- RPG Project/Assets/Scripts/Register.cs
+++ RPG Project/Assets/Scripts/Register.cs
@@ -19,8 +19,58 @@ public class Register : MonoBehaviour
     
     public void OnRegisterClick()
     {
-       
+        // lấy dữ liệu từ input
+        var email = emailInput.text;
+        var password = passwordInput.text;
+        var name = nameInput.text;
+
+        var Account = new Account
+        {
+            Email = email,
+            Password = password,
+            Name = name
+        };
+
+        var json = JsonUtility.ToJson(Account);
+      //  Debug.Log(json);
+        StartCoroutine(Post(json));
     }
 
+    IEnumerator Post(string json)
+    {
+        var url = "https://localhost:44343/api/register";
+        var request = new UnityWebRequest(url, "POST");
+        byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(json);
+        request.uploadHandler = (UploadHandler) new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = (DownloadHandler) new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+
+        yield return request.SendWebRequest();
+
+        if(request.result == UnityWebRequest.Result.ConnectionError ||
+           request.result == UnityWebRequest.Result.ProtocolError)
+        {
+            Debug.Log("POST Error: " + request.error);
+            Debug.Log(request.downloadHandler.text);
+        }
+        else
+        {
+            var response = JsonUtility.FromJson<RegisterModelResponse>(request.downloadHandler.text);
+            Debug.Log(response.status);
+            if (response.status)
+            {
+                Debug.Log("Register success");
+                Debug.Log(response.data.Email);
+                Debug.Log(response.data.Name);
+            }
+            else
+            {
+                Debug.Log("Register fail");
+                Debug.Log(response.data.Email);
+                Debug.Log(response.data.Name);
+
+            }
+        }
+    }
     
 }
